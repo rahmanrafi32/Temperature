485e0e423b6e00e8aa85df6c94a71230ba9922cc
Rafi Rahman <rafi.rahman03@yahoo.com>
2021-02-05 00:16:44 +0600

    solved problem

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.getElementById('submitButton').addEventListener('click', function () {
     const city = document.getElementById('cityName').value;
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=75278c8087bf1d38f9b5cdcc6bba7d9b&units=metric')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=75278c8087bf1d38f9b5cdcc6bba7d9b&units=metric')
         .then(res => res.json())
         .then(data => {
             const temp = data.main.temp;
             const weatherStatus = data.weather[0].main;
+            const weatherIcon = data.weather[0].icon;
+            document.getElementById("weatherImg").setAttribute('src', 'https://openweathermap.org/img/wn/' + weatherIcon + '.png');
             getElement('temp', temp);
             getElement('weather', weatherStatus);
             getElement('city', city);
+
         });
 });
 
